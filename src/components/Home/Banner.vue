48db9ed3eb93a4c4881067accbdc923ff83ecf1b
Mehmet Öztokay <mehmetoztokay@gmail.com>
2021-10-30 11:00:42 +0300

    Home banner duzenlendi.

--- src/components/Home/Banner.vue
+++ src/components/Home/Banner.vue
@@ -26,5 +26,16 @@ export default {
   align-items: center;
   justify-content: center;
   padding: 8rem 0;
+  position: relative;
+}
+.home-banner::after {
+  content: "";
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.2);
+  z-index: -1;
 }
 </style>
